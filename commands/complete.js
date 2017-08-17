7d04744e5bd7cc7278711191a7f5beae089d90cd
John Ware <johnware@Johns-MacBook-Air.local>
2017-08-17 10:32:51 -0700

    complete function created

---
+++ commands/complete.js
@@ -0,0 +1,10 @@
+const client = require('./pg')
+const text = 'UPDATE tasks SET isComplete = true WHERE id=$1'
+//Study process.argv[3]
+const input = [process.argv[3]]
+
+const done = () => {client.query(text, input)
+  .then(res => console.log('Task ' + input.join('') + ' is complete'))
+  .catch(err => console.log(err))}
+
+module.exports = complete
